ab3b4ae144e9303ff21880ca4d99c71a76f79294
ddeveza <ddeveza@ymail.com>
2024-01-03 08:46:08 +0800

    implement clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
       return (
             <div className='h-screen'>
+                  Test
                   <UserButton afterSignOutUrl='/' />
             </div>
       );
