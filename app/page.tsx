4da7d4f03d3e4246e43f492e415ef910bf83f2e7
ddeveza <ddeveza@ymail.com>
2024-01-03 09:59:13 +0800

    completed dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
       return (
             <div className='h-screen'>
-                  Test
                   <UserButton afterSignOutUrl='/' />
             </div>
       );
