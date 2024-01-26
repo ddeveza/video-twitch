587339fb8e057af80935d226f8707cb06a9d39a9
ddeveza <ddeveza@ymail.com>
2024-01-26 11:01:54 +0800

    added tooltip

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
